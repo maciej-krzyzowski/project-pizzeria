142501b306681521c9460cd9fb14c106a2663ba9
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2019-11-21 15:49:00 +0100

    Add initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,58 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+      thisProduct.id = id;
+      thisProduct.data = data;
+      thisProduct.renderInMenu();
+      thisProduct.initAccordion();
+    }
+    renderInMenu(){
+      const thisProduct = this;
+      // wygenerować kod HTML pojedynczego produktu,
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+      // stworzyć element DOM na podstawie tego kodu produktu,
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+      // znaleźć na stronie kontener menu,
+      const menuContainer = document.querySelector(select.containerOf.menu);
+      // wstawić stworzony element DOM do znalezionego kontenera menu.
+      menuContainer.appendChild(thisProduct.element);
+    }
+    initAccordion(){
+
+      const thisProduct = this;
+      const trigger = thisProduct.element.querySelector(select.menuProduct.clickable);
+      trigger.addEventListener('click', function(){
+        event.preventDefault();
+        thisProduct.element.classList.toggle('active');
+        const activeProducts = document.querySelectorAll('.active');
+        for (let activeProduct of activeProducts) {
+          if (activeProduct != thisProduct.element) {
+            console.log(activeProduct)
+            activeProduct.classList.remove('active');
+            console.log('dupadupa')
+          }
+        }
+
+      })
+    }
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+    },
+
+    initData: function(){
+      const thisApp = this;
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,6 +111,9 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
