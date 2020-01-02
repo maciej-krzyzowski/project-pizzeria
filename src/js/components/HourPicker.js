9a8422483e6346131464429263bb4fdde6327587
Maciej Krzyżowski <maciek.krzyzowski44@gmail.com>
2020-01-03 00:44:56 +0100

    Formatting HaourPicker

--- src/js/components/HourPicker.js
+++ src/js/components/HourPicker.js
@@ -1,6 +1,6 @@
 import {BaseWidget} from './BaseWidget.js';
 import {settings, select} from '../settings.js';
-import { utils } from '../utils.js';
+import {utils} from '../utils.js';
 
 export class HourPicker extends BaseWidget {
     constructor(wrapper) {
